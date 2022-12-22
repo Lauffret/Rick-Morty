6dc93f1a5263521ffc1784729f35ed49a5b42c8c
unknown <l.auffret@digikap.com>
2022-12-22 12:30:43 +0100

    ajout page personnages

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,6 @@ import configureStore from './configureStore'
 
 import 'typeface-ibm-plex-sans'
 
-// We use hash history because this example is going to be hosted statically.
-// Normally you would use browser history.
 const history = createBrowserHistory()
 
 const initialState = window.INITIAL_REDUX_STATE
@@ -17,7 +15,4 @@ const store = configureStore(history, initialState)
 
 ReactDOM.render(<Main store={store} history={history} />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister()
